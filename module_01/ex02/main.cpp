5ca6cb327c6a6bf8197ec7fad6c7dfbd0d1d149e
Kaio Matos <kaio_matos22@hotmail.com>
2023-11-09 21:02:42 -0300

    module 01: ex02

---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmatos-s <kmatos-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 19:57:11 by kmatos-s          #+#    #+#             */
+/*   Updated: 2023/11/09 20:59:30 by kmatos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ostream>
+#include <iostream>
+
+int	main(void) {
+	std::string message     = "HI THIS IS BRAIN";
+	std::string *stringPTR  = &message;
+	std::string &stringREF  = message;
+
+	std::cout << "ADDRESSES" << std::endl;
+	std::cout << "message    " << &message << std::endl;
+	std::cout << "stringPTR  " << stringPTR << std::endl;
+	std::cout << "stringREF  " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "VALUE" << std::endl;
+	std::cout << "message    " << message << std::endl;
+	std::cout << "stringPTR  " << *stringPTR << std::endl;
+	std::cout << "stringREF  " << stringREF << std::endl;
+
+	return (0);
+}
